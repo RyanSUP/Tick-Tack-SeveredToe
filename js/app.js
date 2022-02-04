aeafe8f5d8f3f38728cec58ea45b4201d0ff82fe
R Mo <morici23@gmail.com>
2022-02-04 15:15:31 -0500

    Create winningCombinations array

--- js/app.js
+++ js/app.js
@@ -49,14 +49,20 @@ function renderGameStateMessage() {
         gameStatus.textContent = (winState > 0) ? "X wins!" : "O's wins!"
     }
 }
-
-	// 3.3) The render function should:
-	  // [x] 3.3.1) Loop over the board array (which represents the squares on the page), and for each iteration:   
-		  // [x] 3.3.1.1) Use the index of the iteration to access the square in the squares array that corresponds with the current cell being iterated over in the board array
-		  // [x] 3.3.1.2) Style that square however you wish dependant on the value contained in the current cell being iterated over (-1, 1, or null)
-	  // 3.3.2) [ ] Render a message reflecting the currrent game state:
-	    // [x] 3.3.2.1) If winner has a value other than null (game still in progress), render whose turn it is.
-	      // Hint: Maybe use a ternary inside of a template literal here?
-	    // [x] 3.3.2.2) If winner is equal to 'T' (tie), render a tie message.
-	    // [x] 3.3.2.3) Otherwise, render a congratulatory message to which player has won.
-	      // Hint (again): Maybe use a ternary inside a template literal here
+/**
+    [
+        [0, 1, 2]
+        [3, 4, 5]
+        [6, 7, 8]
+    ]
+*/
+const winningCombinations = [
+    [0, 1, 2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
