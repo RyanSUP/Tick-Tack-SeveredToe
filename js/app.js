e010c74f315ce8847f6cf7b451abcda3ca6bec75
R Mo <morici23@gmail.com>
2022-02-04 11:37:21 -0500

    Store cache element references for the board and message elements.

--- js/app.js
+++ js/app.js
@@ -3,12 +3,13 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let boardTiles = []
+let boardSquares = []
 let turn = 1 // 1 = player 1 / -1 = player 2
 let winState = null
 
 /*------------------------ Cached Element References ------------------------*/
-
+const sqrElements = document.querySelectorAll('.sq') // Returns a node-list
+const gameStatus = document.querySelector('#message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
