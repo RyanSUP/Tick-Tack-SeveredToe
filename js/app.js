b8a9eee079dd79fcc011397fe201e77c2fc6b0ad
R Mo <morici23@gmail.com>
2022-02-06 17:53:57 -0500

    Clean up.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
-
-
 /*------------------------ Cached Element References ------------------------*/
 const sqrElements = document.querySelectorAll('.sq') // Returns a node-list
 const message = document.querySelector('#message')
 const boardSection = document.querySelector('.board')
 const replayBtn = document.querySelector('#replay')
-const header = document.querySelector('.game-tokens')
 const ties = document.querySelector('.ties')
 const crown = document.querySelector('.crown')
+const avatars = document.querySelectorAll('.avatar')
+
 /*----------------------------- Event Listeners -----------------------------*/
 boardSection.addEventListener('click', handleBoardClick)
-replayBtn.addEventListener('click', init)
-header.addEventListener('click', setGameToken)
+replayBtn.addEventListener('click', handleReplay)
+// Don't want to take advantage of bubbling with the avatars because it allows the player
+// to select the entire div as an avatar.
+avatars.forEach(avatar => avatar.addEventListener('click', handleSelectAvatar))
 
 
 /*-------------------------------- Constants --------------------------------*/
@@ -33,42 +34,26 @@ const winningCombinations = [
     [2,4,6],
 ]
 
-// Convert htmlCollection to array 
-// (https://www.gavsblog.com/blog/htmlcollection-foreach-loop-convert-object-to-array-javascript)
-const headerChildren = [...header.children]
-
-// This is a janky way around needing more cached references - grab everything in the div and trash what I dont want.
-// I need to fix this.
-headerChildren.pop()
-headerChildren.shift()
-
 /*---------------------------- Variables (state) ----------------------------*/
 // turn 1 = X, turn -1 = O
-let boardSquares, turn, winState, firstPlayerToken, secondPlayerToken
+let boardSquares, turn, winState, player1Avatar, player2Avatar
 
 /*-------------------------------- Main --------------------------------*/
 init()
 
 /*-------------------------------- Functions --------------------------------*/
+
+/*-------------------------------- 🥩  Meat n' Taters 🥔  --------------------------------*/
 function init() {
     // init boardSquares to 9 nulls
     boardSquares = [null, null, null, null, null, null, null, null, null,]
     
-    // Hide the ties (when replay button is pressed)
-    hideTies()
-    hideCrown()
-    setAllHeaderChildrenVisible()
-
     // init tokens
-    makeHeaderChildrenSelectable()
-    firstPlayerToken = null
-    secondPlayerToken = null
+    player1Avatar = null
+    player2Avatar = null
 
     // init turn
     turn = 1 
-
-    // Reset turn indicator (opacity on tokens)
-    resetTurnIndicator()
 
     // init winState to null
     winState = null
@@ -77,103 +62,11 @@ function init() {
     render()
 }
 
-// Gives the game tokens at the top of the screen a class. This lets the players choose again if the game is reset.
-function makeHeaderChildrenSelectable() {
-    headerChildren.forEach(child =>  child.className = 'selectable')
-}
-
-function renderTie() {
-    ties.removeAttribute('hidden')
-}
-
-function hideTies() {
-    ties.setAttribute('hidden', true)
-}
-
-function renderCrown() {
-    crown.removeAttribute('hidden')
-}
-
-function hideCrown() {
-    crown.setAttribute('hidden', true)
-}
-
-function setAllHeaderChildrenVisible() {
-    headerChildren.forEach(child => child.removeAttribute('hidden'))
-}
-
-
-function render() {
-    renderMessage()
-    renderReplayButton()
-    renderBoard()
-}
-
-function renderBoard() {
-    sqrElements.forEach((sqr, idx) => {
-        let boardSqr = boardSquares[idx]
-        if(boardSqr) { // not null
-            sqr.innerHTML = (boardSqr > 0 ) ?  firstPlayerToken : secondPlayerToken
-        } else {
-            sqr.innerHTML = ''
-        }
-    })
-}
-
-function renderTurnIndicator() {
-    headerChildren.forEach(child => {
-        if(turn > 0 ) {
-            child.style.opacity = (child.className.includes('player1') === false) ? '40%' : '100%'
-        } else {
-            child.style.opacity = (child.className.includes('player-1') === false) ? '40%' : '100%'
-        }
-    })
-}
-
-function resetTurnIndicator() {
-    headerChildren.forEach(child => child.style.opacity = '100%')
-}
-
-function renderMessage() {
-    if(firstPlayerToken === null || secondPlayerToken === null) {
-        // New game, show message and wait for players to select tokens
-        renderPlayerSelectMessage()
-    } else if(winState === null) {
-        // Game in progress, hide message
-        message.textContent = ''
-        renderTurnIndicator()
-    } else if(winState === 'T') {
-        // Tie game, show the tie!
-        resetTurnIndicator()
-        message.textContent = 'Tie Game!'
-        renderTie()
-    } else {
-        // Crown the winner!
-        renderCrown()
-        renderWinnerOnly()
-        confetti.start(2000)
-    }
-}
-
-function renderPlayerSelectMessage() {
-    bounceMessage() // Add animated prompt for selecting players.
-    let player = (firstPlayerToken === null) ? 'Player 1' : 'Player 2'
-    message.innerHTML = `${player}, pick a token!` 
-}
-
-function renderReplayButton() {
-    replayBtn.style.visibility = (winState === null) ? 'hidden' : 'visible'
-}
-
-function bounceMessage() {
-    message.classList.add("animate__bounce", "animate__repeat-2", "animate__slow")
-}
-
 function handleBoardClick(eventObject) {
     let id = parseInt(eventObject.target.id.slice(2))
 
     // players haven't picked tokens
-    if(firstPlayerToken === null || secondPlayerToken === null) {
+    if(player1Avatar === null || player2Avatar === null) {
         return
     }
 
@@ -192,18 +85,6 @@ function handleBoardClick(eventObject) {
     render()
 }
 
-function nextTurn() { turn *= -1} 
-
-function renderWinnerOnly() {
-    // if the winstate is 1, hide everything that doesn't have a class of player1
-    let winnerClass = (winState > 0) ? 'player1' : 'player-1'
-    headerChildren.forEach(child => {
-        if(!child.className.includes(winnerClass)) {
-            child.setAttribute('hidden', true)
-        }
-    })
-}
-
 function getWinner() {
     for(let i = 0; i < winningCombinations.length; i++) {
         let total = winningCombinations[i].reduce((prev, idx) => { 
@@ -216,36 +97,159 @@ function getWinner() {
     return (boardSquares.some((square) => square === null)) ? null : 'T'
 }
 
-function setGameToken(eventObject) {
-    // Don't do anything if both players selected a token
-    if(firstPlayerToken && secondPlayerToken) {
+function handleSelectAvatar(eventObject) {
+    // Don't do anything if both players selected a piece
+    if(player1Avatar && player2Avatar) {
         return
     }
 
-    if(eventObject.target.className === 'selectable') {
-        // Remove selectable class from the gameToken, this way it can't be chosen twice.
-        eventObject.target.classList.remove('selectable')
-        // Add the player's turn number to the gameToken as a class. This will be used for the turn indicator and can potentially be used to target the winner's pieces so they animate at the end.
-        eventObject.target.classList.add(`player${turn}`)
+    // Don't let a plater pick an avatar that is already claimed!
+    if(eventObject.target.className.includes('player') === false) {
         
-        // init chosen token
-        let chosenToken = eventObject.target.cloneNode()
-        chosenToken.classList.add("token")
-
-        // Animate selected token needs to be done last so chosenToken doesn't copy the animation class.
-        eventObject.target.classList.add('animate__heartBeat')
-    
-        
-        // Set token to player
+        // Copy the HTML of the selected piece
+        let selectedAvatar = eventObject.target.cloneNode()
+        selectedAvatar.classList.add("player-avatar")
         if(turn > 0) {
-            firstPlayerToken = chosenToken.outerHTML
+            player1Avatar = selectedAvatar.outerHTML
         } else {
-            secondPlayerToken = chosenToken.outerHTML
+            player2Avatar = selectedAvatar.outerHTML
         }
         
+        // Animate selected token needs to be done last so selectedAvatar doesn't copy the animation class,
+        // otherwise every time a player puts a piece on the board it will animate.
+        // Add the player's turn number to the avatar as a class. Needed by helper functions.
+        eventObject.target.classList.add("animate__heartBeat", `player${turn}`)
 
-        // Go to the next turn so player 2 can select their token.
+        // After animation, indicate to the other play that an avatar can't be picked
+        setTimeout(() => eventObject.target.style.opacity = '40%', 700);
+        
+        // Go to the next turn so player 2 can select their avatar.
         nextTurn()
     }
     render()
 }
+
+/*-------------------------------- 🔌 Reseting the game 💾 --------------------------------*/
+
+function handleReplay() {
+    resetHTML()
+    // Animations break on the next replay without this quick delay. Something to do with the event loop?
+    setTimeout(() => {
+        wobbleBoard()
+        init()
+    }, 5)
+}
+
+function resetHTML() {
+    // Remove animation classes
+    message.className = ""
+    boardSection.className = "board"
+    ties.className = "ties"
+    // Reset avatars
+    turnOffTurnIndicator()
+    avatars.forEach(avatar => {
+        avatar.className="avatar" // strip a player's claim to the avatar
+        avatar.removeAttribute('hidden') // Show all avatars (Losers get hidden)
+    })
+    // Hide avatar accesories
+    crown.setAttribute('hidden', true)
+    ties.setAttribute('hidden', true)
+    // Hide the replay
+    replayBtn.style.visibility = 'hidden'
+
+}
+
+/*-------------------------------- 🖥  Rendering 🧾 --------------------------------*/
+function render() {
+    if(player1Avatar === null || player2Avatar === null) {
+        // New game, show message and wait for players to select tokens
+        renderPlayerSelectMessage()
+    } else if(winState === null) {
+        // Game in progress, hide message
+        message.textContent = ''
+        showTurnIndicator()
+    } else if(winState === 'T') {
+        // Tie game, show the ties!
+        renderTieScreen()
+    } else {
+        // Winner!
+        renderWinScreen()
+    }
+    renderBoard()
+}
+
+function renderPlayerSelectMessage() {
+    bounceMessage() // Add animated prompt for selecting players.
+    let player = (player1Avatar === null) ? 'Player 1' : 'Player 2'
+    message.innerHTML = `${player}, pick a token!` 
+}
+
+function renderTieScreen() { 
+    message.textContent = 'Tie Game!'
+    replayBtn.style.visibility = 'visible'
+    turnOffTurnIndicator()
+    // Don the ties!  👔  👔  👔 
+    ties.removeAttribute('hidden') 
+    ties.classList.add("animate__animated", "animate__fadeInDown")
+}
+
+function renderWinScreen() {
+    hideLosingPieces() // This makes crowning the winning piece easier since the winner will always be centered. (No one wants to see the loser anyway)
+    replayBtn.style.visibility = 'visible'
+    // Commence coronation! 👑
+    crown.removeAttribute('hidden')
+    confetti.start(2000)
+}
+
+function renderBoard() {
+    sqrElements.forEach((sqr, idx) => {
+        let boardSqr = boardSquares[idx]
+        if(boardSqr) { // not null
+            sqr.innerHTML = (boardSqr > 0 ) ?  player1Avatar : player2Avatar
+        } else {
+            sqr.innerHTML = ''
+        }
+    })
+}
+
+/*-------------------------------- 👷‍♂️ Helpers 🏗 --------------------------------*/
+function bounceMessage() {
+    message.classList.add(
+        "animate__animated",
+        "animate__bounce", 
+        "animate__repeat-2", 
+        "animate__slow"
+    )
+}
+
+function wobbleBoard() {
+    boardSection.classList.add(
+        "animate__animated",
+        "animate__wobble", 
+        "animate__fast"
+    )
+}
+
+function hideLosingPieces() {
+    let winnerClass = (winState > 0) ? 'player1' : 'player-1'
+    avatars.forEach(avatar => {
+        // Hide if not the winner
+        if(!avatar.className.includes(winnerClass)) {
+            avatar.setAttribute('hidden', true)
+        }
+    })
+}
+
+function showTurnIndicator() {
+    avatars.forEach(avatar => {
+        if(turn > 0 ) {
+            avatar.style.opacity = (avatar.className.includes('player1') === false) ? '40%' : '100%'
+        } else {
+            avatar.style.opacity = (avatar.className.includes('player-1') === false) ? '40%' : '100%'
+        }
+    })
+}
+
+function turnOffTurnIndicator() { avatars.forEach(avatar => avatar.style.opacity = '100%')}
+
+function nextTurn() { turn *= -1} 
